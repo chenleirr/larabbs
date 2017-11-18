0f4212723c764d67378d121f7fcc43490f2bbb11
Rayc <350624553@qq.com>
2017-11-18 06:59:09 +0000

    Basic user profile editing

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'PagesController@root')->name('root');
 
-//Auth::routes();//等同于以下
+//Auth::routes();//等同于以下=====================
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
@@ -28,6 +28,12 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+//等同于以下结束=====================
 
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+//等同于以下=====================
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+//Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+//等同于以下结束=====================
