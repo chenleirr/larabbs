b0b6063a669259ace86c09383526b9f48d037f40
Rayc <350624553@qq.com>
2017-11-22 07:47:47 +0000

    Create categories

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'introduction', 'avatar',
+        'name',
+        'email',
+        'password',
+        'introduction',//简介
+        'avatar',//头像地址
     ];
 
     /**
